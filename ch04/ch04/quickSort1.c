f0b3ea13ae93f677dd31a90cb0cef6f04cdd1c85
leminse <s2330@e-mirim.hs.kr>
2024-09-02 14:55:59 +0900

    퀵 정렬1

--- ch04/ch04/quickSort1.c
+++ ch04/ch04/quickSort1.c
@@ -23,9 +23,6 @@ int main(void) {
 	int a[] = { 15, 7, 2, 20, 33, 17, 26, 5, 21, 30, 27, 29, 11, 10, 3 };
 	int i, n = sizeof(a) / sizeof(int);
 
-	//printf("정렬 전 : ");
-	//for (i = 0; i < n; i++) printf("%5d", a[i]);
-
 	QuickSort(a, 0, n - 1);
 
 	//printf("\n퀵 정렬 후 : ");
